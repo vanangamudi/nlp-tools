f47f98e37fde19bf0eb2ad8fe3a9adb4ba7b5155
M Selvakumar <selva.developer@gmail.com>
2018-04-15 16:03:00 +0530

    __delitem__ works, __iter__ returns an iterator instead of list, index() stop=None instead of -1; adds watchdog to avoid infinite looping; handles zero offsets; replace() uses slices build from indices()

--- tokenstring.py
+++ tokenstring.py
@@ -4,7 +4,7 @@ import logging
 from pprint import pprint, pformat
 logging.basicConfig(format="%(levelname)-8s:%(filename)s.%(funcName)20s >>   %(message)s")
 log = logging.getLogger(__name__)
-log.setLevel(logging.DEBUG)
+log.setLevel(logging.INFO)
 
 class TokenString(list):
 
@@ -49,10 +49,7 @@ class TokenString(list):
             self.tokenized_string[key] = value
 
     def __delitem__(self, key):
-        if isinstance(value, TokenString):
-            del self.tokenized_string[key]
-        elif isinstance(value, list):
-            del self.tokenized_string[key]
+        del self.tokenized_string[key]
     
     def __repr__(self):
         return 'TokenString({})'.format(self.tokenized_string)
@@ -61,9 +58,12 @@ class TokenString(list):
         return len(self.tokenized_string)
     
     def __iter__(self):
-        return self.tokenized_string
+        return self.tokenized_string.__iter__()
 
-    def index(self, subspan, start_at=0, stop_at=-1):
+    def index(self, subspan, start_at=0, stop_at=None):
+        watchdog = 0
+        if not stop_at:
+            stop_at = len(self)
         window = self.tokenized_string[start_at: stop_at]
         if isinstance(subspan, str):
             subspan = self.tokenize(subspan)
@@ -78,20 +78,28 @@ class TokenString(list):
                 log.debug('found subspan[0] at {}'.format(start_idx))
                 offset = 0
                 while start_idx + offset < len(window) and offset < len(subspan) and window[start_idx+offset] == subspan[offset]:
+                    log.debug('{} == {}'.format(window[start_idx+offset], subspan[offset]))
+                    log.debug('{}, {}'.format(start_idx, offset))
                     offset += 1
-
+                    watchdog += 1
+                    if watchdog > len(window):
+                        log.error(pformat(locals()))
+                        return
+                    
                 if offset == len(subspan):
                     log.debug('subspan found : {}, {}'.format(start_at+start_idx,
                                                               start_at+start_idx+offset))
                     return (start_at+start_idx, start_at+start_idx+offset)
                 
                 if not subspan[0] in window[start_idx+offset:]:
+                    log.debug(locals())
                     log.debug('subspan not found after start_idx: {}'.format(start_idx+1))
                     log.debug(pformat(window[start_idx+offset:]))
                     return
- 
-                start_idx += window[start_idx + offset:].index(subspan[0])
-                
+
+                start_idx += offset
+                start_idx += window[start_idx:].index(subspan[0])
+
     
     def indices(self, item):
         _indices = []
@@ -107,9 +115,26 @@ class TokenString(list):
     def replace(self, key, value, all=False):
         if all:
             for i in self.indices(key):
-                self.tokenized_string[i] = value
+                self.tokenized_string[slice(*i)] = value
 
         else:
             i = self.index(key)
             if i:
-                self.tokenized_string[i]  = value   
+                self.tokenized_string[slice(*i)]  = value   
+
+
+    def delete_whitespace(self):
+        self.tokenized_string = [i for i in self.tokenized_string
+                                 if not i.isspace()]
+
+        return self
+
+
+import sys
+from tokenizer import word_tokenize
+if __name__ == '__main__':
+
+    t = TokenString(sys.argv[1], word_tokenize).delete_whitespace()
+    s = TokenString(sys.argv[2], word_tokenize).delete_whitespace()
+
+    print(s in t)
